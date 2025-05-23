7e4a865932a77dadb7d1baed08b1faf85910e777
sonic-arrowtails <peculiarmechanicalspydevice@gmail.com>
2025-05-23 20:18:07 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.params import Body
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "welkem to my api"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "this is your post)"}
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title: {payload["title"]}; content: {payload["content"]}"}
+
