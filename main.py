9700002303a612f3bc7bbf4cbec1094f56a67412
sonic-arrowtails <peculiarmechanicalspydevice@gmail.com>
2025-05-23 21:18:17 +0900

    pydantic model + schema for http post validation

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True  # default to true
+    rating: Optional[int] = None  # None isnt an int so use optional
 
 @app.get("/")
 def root():
@@ -12,7 +20,12 @@ def get_posts():
     return {"data": "this is your post)"}
 
 @app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title: {payload["title"]}; content: {payload["content"]}"}
+def create_posts(new_post: Post): # schema, validation
+    print(new_post)  # title='top beches in florida' content='yo chek this out'
+    print(new_post.title)
+    print(new_post.content)
+    print(new_post.published)
+
+    print(new_post.model_dump())  #used to be dict()
+    return {"new_post": "new post"}
 
