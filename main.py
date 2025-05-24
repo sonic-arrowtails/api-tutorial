18b9612723264fd0e155db30fab0a91cf018e453
sonic-arrowtails <peculiarmechanicalspydevice@gmail.com>
2025-05-24 20:46:12 +0900

    Naming convention of app.post, get posts from glob var

--- main.py
+++ main.py
@@ -11,21 +11,24 @@ class Post(BaseModel):
     published: bool = True  # default to true
     rating: Optional[int] = None  # None isnt an int so use optional
 
+# global variable to store posts
+my_posts = [{"title": "Title of post1", "content": "Content of post1", "id":1},
+            {"title": "Favourite foods", "content": "i liek pizza", "id":2}]
+
 @app.get("/")
 def root():
     return {"message": "welkem to my api"}
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "this is your post)"}
+    return {"data": my_posts} #fastapi changes arrays to json
 
-@app.post("/createposts")
-def create_posts(new_post: Post): # schema, validation
-    print(new_post)  # title='top beches in florida' content='yo chek this out'
-    print(new_post.title)
-    print(new_post.content)
-    print(new_post.published)
-
-    print(new_post.model_dump())  #used to be dict()
-    return {"new_post": "new post"}
+@app.post("/posts") # naming convention
+def create_posts(post: Post): # schema, validation
+    print(post)  # title='top beches in florida' content='yo chek this out'
+    print(post.title)
+    print(post.content)
+    print(post.published)
+    print(post.model_dump())  #used to be dict()
+    return {"post": post}
 
