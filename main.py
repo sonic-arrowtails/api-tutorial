20943e84d3b8d83f731cfbe29e1394d9c1401386
sonic-arrowtails <peculiarmechanicalspydevice@gmail.com>
2025-05-29 13:51:58 +0900

    404 not found exception and 201 created status code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -12,7 +12,6 @@ class Post(BaseModel):
     published: bool = True  # default to true
     rating: Optional[int] = None  # None isnt an int so use optional
 
-# global variable to store posts
 my_posts = [{"title": "Title of post1", "content": "Content of post1", "id":1},
             {"title": "Favourite foods", "content": "i liek pizza", "id":2}]
 
@@ -29,14 +28,19 @@ def root():
 def get_posts():
     return {"data": my_posts} #fastapi changes arrays to json
 
-@app.post("/posts") # naming convention
+@app.post("/posts", status_code = status.HTTP_201_CREATED)
 def create_posts(post: Post): # schema, validation
     post_dict = post.model_dump()
     post_dict["id"] = randrange(0,100000000000)
     my_posts.append(post_dict)
     return {"post": post_dict}
 
-@app.get("/posts/{id}")
-def get_post(id: int):  # converts to int
+@app.get("/posts/{id}") # however if we have a /posts/latest or smthing, it will call this function. routing error. order matters here
+def get_post(id: int):  # response:Response
     post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id {id} was not found")
+        # response.status_code = status.HTTP_404_NOT_FOUND  # no hardcoding in a number
+        # return {"messgae":f"post with id {id} was not found"}
     return {"post": post}
