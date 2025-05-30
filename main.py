b9d90ad467eaccc1d3f0a94140b9958dd050c1e3
sonic-arrowtails <peculiarmechanicalspydevice@gmail.com>
2025-05-30 19:44:37 +0900

    update post. Finished CRUD operations

--- main.py
+++ main.py
@@ -30,16 +30,16 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": my_posts} #fastapi changes arrays to json
+    return {"data": my_posts}
 
 @app.post("/posts", status_code = status.HTTP_201_CREATED)
-def create_posts(post: Post): # schema, validation
+def create_posts(post: Post):
     post_dict = post.model_dump()
     post_dict["id"] = randrange(0,100000000000)
     my_posts.append(post_dict)
     return {"post": post_dict}
 
-@app.get("/posts/{id}") # however if we have a /posts/latest or smthing, it will call this function. routing error. order matters here
+@app.get("/posts/{id}")
 def get_post(id: int):  # response:Response
     post = find_post(id)
     if not post:
@@ -52,8 +52,19 @@ def get_post(id: int):  # response:Response
 @app.delete("/posts/{id}", status_code= status.HTTP_204_NO_CONTENT)
 def delete_post(id:int):
     index = find_index_post(id)
-    if not index:
+    if index == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id {id} was not found")
     post = my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id:int,post:Post):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id {id} was not found")
+    post_dict = post.model_dump()
+    post_dict["id"] = id
+    my_posts[index] = post_dict
+    return {"data" : post_dict}
