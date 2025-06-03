ed06b2e590cc17c066dcdec4ab373e7bcf310f09
sonic-arrowtails <peculiarmechanicalspydevice@gmail.com>
2025-06-04 08:33:38 +0900

    sql get posts, create post

--- app/main.py
+++ app/main.py
@@ -43,14 +43,17 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": my_posts}
+    cursor.execute("""SELECT * FROM posts """)
+    posts = cursor.fetchall()
+    return {"data": posts}
 
 @app.post("/posts", status_code = status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    post_dict = post.model_dump()
-    post_dict["id"] = randrange(0,100000000000)
-    my_posts.append(post_dict)
-    return {"post": post_dict}
+    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
+        (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"post": new_post}
 
 @app.get("/posts/{id}")
 def get_post(id: int):  # response:Response
