d899cc713fc5642f0a5b67de74f2726e54ac6a2f
sonic-arrowtails <peculiarmechanicalspydevice@gmail.com>
2025-06-13 16:00:47 +0900

    user table and create user

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from datetime import datetime
 
 class PostBase(BaseModel):
@@ -15,3 +15,14 @@ class Post(PostBase):
     
     class Config:
         from_attributes = True # orm_mode
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+    class Config:
+        from_attributes = True
