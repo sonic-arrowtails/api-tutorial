9fbefa5634917d2a97d393dc535d34e8388c14d5
sonic-arrowtails <peculiarmechanicalspydevice@gmail.com>
2025-06-13 16:59:22 +0900

    fasapi routers

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ import time
 from . import models, schemas, utils
 from .database import engine, get_db
 from sqlalchemy.orm import Session
+from.routers import post, user
 
 models.Base.metadata.create_all(bind=engine)
     
@@ -24,83 +25,12 @@ while True:
         print("Error:", error)
         time.sleep(3)
 
-my_posts = [{"title": "Title of post1", "content": "Content of post1", "id":1},
-            {"title": "Favourite foods", "content": "i liek pizza", "id":2}]
+# my_posts = [{"title": "Title of post1", "content": "Content of post1", "id":1},
+#             {"title": "Favourite foods", "content": "i liek pizza", "id":2}]
+
+app.include_router(post.router)
+app.include_router(user.router)
 
 @app.get("/")
 def root():
     return {"message": "welkem to my api"}
-
-@app.get("/posts", response_model = List[schemas.Post])
-def test_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all() 
-    # cursor.execute("""SELECT * FROM posts  ORDER BY id ASC""")
-    # posts = cursor.fetchall()
-    return posts
-
-
-@app.post("/posts", status_code = status.HTTP_201_CREATED,response_model = schemas.Post)
-def create_posts(post: schemas.PostCreate,db: Session = Depends(get_db)):
-    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
-    #     (post.title, post.content, post.published))
-    # new_post = cursor.fetchone()
-    # conn.commit()
-    new_post = models.Post(**post.model_dump())
-        # title=post.title, content=post.content, published=post.published
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-    return new_post
-
-@app.get("/posts/{id}",response_model = schemas.Post)
-def get_post(id: int,db: Session = Depends(get_db)):  # response:Response
-    # cursor.execute("""SELECT * FROM posts WHERE id = %s""",(id,))
-    # post = cursor.fetchone()
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id {id} was not found")
-    return post
-
-@app.delete("/posts/{id}", status_code= status.HTTP_204_NO_CONTENT)
-def delete_post(id:int,db: Session = Depends(get_db)):
-    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (id,))
-    # deleted_post = cursor.fetchone()
-    post = db.query(models.Post).filter(models.Post.id == id)
-    if post.first() == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id {id} was not found")
-    post.delete(synchronize_session=False)
-    db.commit()
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-@app.put("/posts/{id}",response_model = schemas.Post)
-def update_post(id:int,post:schemas.PostCreate,db: Session = Depends(get_db)):
-    # cursor.execute("""UPDATE posts SET (title, content, published) = (%s, %s, %s) WHERE id = %s RETURNING *""", 
-    #                (post.title, post.content, post.published, id))
-    # post = cursor.fetchone()
-    # conn.commit()
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-    if post_query.first() == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id {id} was not found")
-    post_query.update(post.model_dump(),synchronize_session=False)
-    db.commit()
-    return post_query.first()
-
-@app.post("/users", status_code = status.HTTP_201_CREATED, response_model = schemas.UserOut)
-def create_posts(user: schemas.UserCreate, db: Session = Depends(get_db)): #dosnt validate repeated email
-    hashed_password = utils.hash(user.password)
-    user.password = hashed_password
-    new_user = models.User(**user.model_dump())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
-
-@app.get("/users/{id}", response_model = schemas.UserOut)
-def get_user(id:int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.Post.id == id).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"user with id {id} not found")
-    return user
---
+++ app/routers/post.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
+from .. import models, schemas
+from ..database import get_db
+from sqlalchemy.orm import Session
+from typing import List
+
+router = APIRouter()
+
+@router.get("/posts", response_model = List[schemas.Post])
+def test_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all() 
+    # cursor.execute("""SELECT * FROM posts  ORDER BY id ASC""")
+    # posts = cursor.fetchall()
+    return posts
+
+
+@router.post("/posts", status_code = status.HTTP_201_CREATED,response_model = schemas.Post)
+def create_posts(post: schemas.PostCreate,db: Session = Depends(get_db)):
+    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
+    #     (post.title, post.content, post.published))
+    # new_post = cursor.fetchone()
+    # conn.commit()
+    new_post = models.Post(**post.model_dump())
+        # title=post.title, content=post.content, published=post.published
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
+
+@router.get("/posts/{id}",response_model = schemas.Post)
+def get_post(id: int,db: Session = Depends(get_db)):  # response:Response
+    # cursor.execute("""SELECT * FROM posts WHERE id = %s""",(id,))
+    # post = cursor.fetchone()
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id {id} was not found")
+    return post
+
+@router.delete("/posts/{id}", status_code= status.HTTP_204_NO_CONTENT)
+def delete_post(id:int,db: Session = Depends(get_db)):
+    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (id,))
+    # deleted_post = cursor.fetchone()
+    post = db.query(models.Post).filter(models.Post.id == id)
+    if post.first() == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id {id} was not found")
+    post.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@router.put("/posts/{id}",response_model = schemas.Post)
+def update_post(id:int,post:schemas.PostCreate,db: Session = Depends(get_db)):
+    # cursor.execute("""UPDATE posts SET (title, content, published) = (%s, %s, %s) WHERE id = %s RETURNING *""", 
+    #                (post.title, post.content, post.published, id))
+    # post = cursor.fetchone()
+    # conn.commit()
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    if post_query.first() == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id {id} was not found")
+    post_query.update(post.model_dump(),synchronize_session=False)
+    db.commit()
+    return post_query.first()
