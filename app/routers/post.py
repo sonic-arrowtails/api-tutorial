edeb1194014791e9939df6a6f5ecce1bbc9f1614
sonic-arrowtails <peculiarmechanicalspydevice@gmail.com>
2025-06-13 17:06:27 +0900

    APIRouter prefix an tags

--- app/routers/post.py
+++ app/routers/post.py
@@ -4,9 +4,12 @@ from ..database import get_db
 from sqlalchemy.orm import Session
 from typing import List
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/posts",
+    tags=["Posts"]
+)
 
-@router.get("/posts", response_model = List[schemas.Post])
+@router.get("/", response_model = List[schemas.Post])
 def test_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all() 
     # cursor.execute("""SELECT * FROM posts  ORDER BY id ASC""")
@@ -14,7 +17,7 @@ def test_posts(db: Session = Depends(get_db)):
     return posts
 
 
-@router.post("/posts", status_code = status.HTTP_201_CREATED,response_model = schemas.Post)
+@router.post("/", status_code = status.HTTP_201_CREATED,response_model = schemas.Post)
 def create_posts(post: schemas.PostCreate,db: Session = Depends(get_db)):
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
     #     (post.title, post.content, post.published))
@@ -27,7 +30,7 @@ def create_posts(post: schemas.PostCreate,db: Session = Depends(get_db)):
     db.refresh(new_post)
     return new_post
 
-@router.get("/posts/{id}",response_model = schemas.Post)
+@router.get("/{id}",response_model = schemas.Post)
 def get_post(id: int,db: Session = Depends(get_db)):  # response:Response
     # cursor.execute("""SELECT * FROM posts WHERE id = %s""",(id,))
     # post = cursor.fetchone()
@@ -37,7 +40,7 @@ def get_post(id: int,db: Session = Depends(get_db)):  # response:Response
                             detail=f"post with id {id} was not found")
     return post
 
-@router.delete("/posts/{id}", status_code= status.HTTP_204_NO_CONTENT)
+@router.delete("/{id}", status_code= status.HTTP_204_NO_CONTENT)
 def delete_post(id:int,db: Session = Depends(get_db)):
     # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (id,))
     # deleted_post = cursor.fetchone()
@@ -49,7 +52,7 @@ def delete_post(id:int,db: Session = Depends(get_db)):
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@router.put("/posts/{id}",response_model = schemas.Post)
+@router.put("/{id}",response_model = schemas.Post)
 def update_post(id:int,post:schemas.PostCreate,db: Session = Depends(get_db)):
     # cursor.execute("""UPDATE posts SET (title, content, published) = (%s, %s, %s) WHERE id = %s RETURNING *""", 
     #                (post.title, post.content, post.published, id))
--- app/routers/user.py
+++ app/routers/user.py
@@ -3,9 +3,12 @@ from .. import models, schemas, utils
 from ..database import get_db
 from sqlalchemy.orm import Session
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/users",
+    tags=["Users"]
+)
 
-@router.post("/users", status_code = status.HTTP_201_CREATED, response_model = schemas.UserOut)
+@router.post("/", status_code = status.HTTP_201_CREATED, response_model = schemas.UserOut)
 def create_posts(user: schemas.UserCreate, db: Session = Depends(get_db)): #dosnt validate repeated email
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
@@ -15,7 +18,7 @@ def create_posts(user: schemas.UserCreate, db: Session = Depends(get_db)): #dosn
     db.refresh(new_user)
     return new_user
 
-@router.get("/users/{id}", response_model = schemas.UserOut)
+@router.get("/{id}", response_model = schemas.UserOut)
 def get_user(id:int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.Post.id == id).first()
     if not user:
