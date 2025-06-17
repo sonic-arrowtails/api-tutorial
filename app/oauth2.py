43a0393c4e8aa2dfd419380f86515a10454396e8
sonic-arrowtails <peculiarmechanicalspydevice@gmail.com>
2025-06-17 13:30:54 +0900

    oauth2 stuff, get_current_user deendency setup, nvalid credentials exception

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,15 +1,37 @@
 from jose import JWTError, jwt
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
+from . import schemas
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 
 SECRET_KEY = "12a26e3b2ffa1fdd14304ced9e550a2977167f72b165624b99ebd67d37bc4cb2"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
+
 def create_access_token(data:dict):
     to_encode = data.copy()
-    expire = datetime.now() + timedelta(minutes=30)
+    expire = datetime.now(datetime.timezone.utc) + timedelta(minutes=30)
     to_encode.update({"exp":expire})
 
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=[ALGORITHM])
 
     return encoded_jwt
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithm=[ALGORITHM])
+        id : str = payload.get("user_id")
+        if id == None: raise credentials_exception
+        token_data = schemas.TokenData(id=id)
+        
+    except JWTError: raise credentials_exception
+
+    return token_data
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+                                         detail = "Could not validate credentials",
+                                         headers= {"WWW-Authenticate": "Bearer"})
+    return verify_access_token(token,credentials_exception)
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,16 +1,18 @@
 from fastapi import Depends, APIRouter, status, HTTPException
 from .. import schemas, database, models, utils, oauth2
 from sqlalchemy.orm import Session
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 router = APIRouter(tags=['authentication'])
 
 @router.post("/login")
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
+    
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='invalid credentials')
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='invalid credentials')
     if not utils.verify(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='invalid credentials')
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='invalid credentials')
     
     access_token = oauth2.create_access_token(data={"user_id":user.id})
 
