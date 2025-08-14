8f6f96346becb181568bd0dc312a9952f70c1df7
sonic-arrowtails <peculiarmechanicalspydevice@gmail.com>
2025-08-14 19:02:03 +0300

    pytest fixtures

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,6 +1,14 @@
 import pytest
 from app.calculations import add, subtract, multiply, divide, BankAccount
 
+@pytest.fixture
+def zero_bank_account():
+    return BankAccount()
+
+@pytest.fixture
+def bank_account():
+    return BankAccount(50)
+
 @pytest.mark.parametrize("a, b, expected",[(1,2,3),(4,5,9),(9,10,19)])
 def test_add(a, b, expected):
     assert add(a,b) == expected
@@ -15,25 +23,26 @@ def test_divide():
     assert divide(6,3) == 2
 
 
-def test_bank_ac_init():
-    joe = BankAccount(50)
-    assert joe.balance == 50
+def test_bank_ac_init(bank_account):
+    assert bank_account.balance == 50
 
-def test_bank_ac_default():
-    joe = BankAccount()
-    assert joe.balance == 0
+def test_bank_ac_default(zero_bank_account):
+    assert zero_bank_account.balance == 0
 
-def test_withdraw():
-    joe = BankAccount(50)
-    joe.withdraw(20)
-    assert joe.balance == 30
+def test_withdraw(bank_account):
+    bank_account.withdraw(20)
+    assert bank_account.balance == 30
 
-def test_deposit():
-    joe = BankAccount(50)
-    joe.deposit(30)
-    assert joe.balance == 80
+def test_deposit(bank_account):
+    bank_account.deposit(30)
+    assert bank_account.balance == 80
 
-def test_collect_interect():
-    joe = BankAccount(50)
-    joe.collect_interest()
-    assert round(joe.balance,5) == 55
+def test_collect_interect(bank_account):
+    bank_account.collect_interest()
+    assert round(bank_account.balance,5) == 55
+
+@pytest.mark.parametrize("dep, withd, expected",[(3,2,1),(200,100,100),(10000,1,9999),(55555,5,55550)])
+def test_stuff(zero_bank_account, dep, withd, expected):
+    zero_bank_account.deposit(dep)
+    zero_bank_account.withdraw(withd)
+    assert zero_bank_account.balance == expected
